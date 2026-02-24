a18bae6e6d2da577ac84e15e96854e7d2114f07f
Miguel <martinezpardomiguelangel@gmail.com>
2026-02-24 20:18:57 +0100

    imprimir info

--- cliente.php
+++ cliente.php
@@ -1,4 +1,7 @@
 <?php 
 $json = file_get_contents("http://localhost/servizo.php");
 $datos = json_decode($json, true);
+echo "<h1>Información do Servizo</h1>";
+echo "<p>Hora: {$datos['hora']} </p>";
+echo "<p>Servidor: {$datos['servidor']}</p>";
 ?>
